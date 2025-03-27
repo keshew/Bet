88afade4bf9f3ad656ab550b047d75199c599745
keshew <artyom.korotckov2017@yandex.ru>
2025-03-27 15:55:19 +0500

    Initial Commit

---
+++ Bet/BetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetApp.swift
+//  Bet
+//
+//  Created by Артём Коротков on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
